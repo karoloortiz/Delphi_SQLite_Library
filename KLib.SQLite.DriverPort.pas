4a13439fcbbc7ceafffb8bd7bb856dbb12022701
Karol Ortiz <zitrokarol@gmail.com>
2022-04-11 16:49:07 +0200

    init

---
+++ KLib.SQLite.DriverPort.pas
@@ -0,0 +1,117 @@
+{
+  KLib Version = 2.0
+  The Clear BSD License
+
+  Copyright (c) 2020 by Karol De Nery Ortiz LLave. All rights reserved.
+  zitrokarol@gmail.com
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted (subject to the limitations in the disclaimer
+  below) provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+  * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+  THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+}
+
+unit KLib.SQLite.DriverPort;
+
+interface
+
+uses
+  KLib.SQLite.FireDac,
+  KLib.Constants;
+
+type
+  TQuery = class(T_Query)
+  public
+    procedure refreshKeepingPosition;
+  end;
+
+  TConnection = class(T_Connection)
+  end;
+
+procedure backup(sourceDatabase: string; destinationDatabase: string);
+
+procedure refreshQueryKeepingPosition(query: TQuery);
+
+function getTQuery(connection: TConnection; sqlText: string = ''; silentMode: boolean = true): TQuery;
+
+function getValidSQLiteTConnection(database: string; password: string = EMPTY_STRING): TConnection;
+function getSQLiteTConnection(database: string; password: string = EMPTY_STRING): TConnection;
+
+implementation
+
+uses
+  KLib.SQLite.Validate,
+  Data.DB;
+
+procedure backup(sourceDatabase: string; destinationDatabase: string);
+begin
+  KLib.SQLite.FireDac.backup(sourceDatabase, destinationDatabase);
+end;
+
+procedure TQuery.refreshKeepingPosition;
+begin
+  refreshQueryKeepingPosition(self);
+end;
+
+procedure refreshQueryKeepingPosition(query: TQuery);
+var
+  _bookmark: TBookmark;
+begin
+  _bookmark := Query.GetBookmark;
+  query.Refresh;
+  query.GotoBookmark(_bookmark);
+end;
+
+function getTQuery(connection: TConnection; sqlText: string = ''; silentMode: boolean = true): TQuery;
+var
+  query: TQuery;
+begin
+  query := TQuery.create(nil);
+  query.connection := connection;
+  query.SQL.Clear;
+  query.SQL.Text := sqlText;
+  query.ResourceOptions.SilentMode := silentMode;
+  Result := query;
+end;
+
+function getValidSQLiteTConnection(database: string; password: string = EMPTY_STRING): TConnection;
+var
+  connection: TConnection;
+begin
+  validateSQLiteCredentials(database, password); //TODO
+  connection := getSQLiteTConnection(database, password);
+  Result := connection;
+end;
+
+function getSQLiteTConnection(database: string; password: string = EMPTY_STRING): TConnection;
+var
+  connection: T_Connection;
+begin
+  connection := _getSQLiteTConnection(database, password);
+  Result := TConnection(connection);
+end;
+
+end.
---
+++ README.md
@@ -0,0 +1,19 @@
+# Delphi_SQLite_Library
+
+KLib Version = 2.0
+
+A library with some useful SQLite Utilities for your Delphi.
+
+Dependencies:
+ - https://github.com/karoloortiz/Delphi_Utils_Library.git
+ - https://github.com/karoloortiz/Delphi_Async_Library.git
+ - FireDAC (avaliable in Delphi RIO Community Edition)
+  
+Functions:
+- useful queries
+
+TODO:
+  - examples
+
+
+If you need support, add a star to the repository and don't hesitate to contact me at zitrokarol@gmail.com
